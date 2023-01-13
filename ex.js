4a5474941592604bdc4d34668f231c1e7bcf085f
Neill10 <95107962+Neill10@users.noreply.github.com>
2023-01-13 14:03:40 -0500

    added POST request ( doesnt work )

--- ex.js
+++ ex.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     res.send('Hello there');
 });
+
 
 
 const courses = [
@@ -15,7 +17,7 @@ const courses = [
 app.get('/api/courses', (req,res)=>{
     res.send(courses);
 });
-
+//HTTP GET requests
 app.get('/api/courses/:id',(req, res)=>{
     const course = courses.find(c=> c.id === parseInt(req.params.id));
     if(!course){
@@ -23,6 +25,30 @@ app.get('/api/courses/:id',(req, res)=>{
     }
     res.send(course);
 })
+
+//HTTP POST requests
+app.post('api/courses', (req,res) => {
+    console.log(req.body.name);
+    // you write the if code here
+    //add an if statement so that the name of the course you post is .min(3) characters 
+    if(req.min(3)){
+        res.status(404).send("Must be more than 3 characters");
+    }
+    else{
+        const course ={
+            //we assign an ID and a name property
+            id: course.length +1,
+            name: req.body.name
+        }
+        //YOU WRITE THE NEXT LINES OF code
+        //next step: push it to the array
+        //next step: the server should return the new resource to the client in the body of the response
+        courses.push(course);
+        res.send(courses);
+        
+    }
+});
+    
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000 ... ");
